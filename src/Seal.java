4c62ebba63d47f687b5b89887b9fcf8f92279388
Nina <nina_gonzalez@dlsu.edu.ph>
2026-06-22 19:36:05 +0800

    added seal abstract class

---
+++ src/Seal.java
@@ -0,0 +1,33 @@
+public class Seal extends Caniformia{
+    @Override
+    public void makeSound(){
+
+        System.out.println("Urgh!");
+    }
+
+    @Override
+    public void sleep(){
+
+        System.out.println("Seal slept for 10 hours today.");
+    }
+
+    @Override
+    public void eat(){
+
+        System.out.println("Seal ate fish today!");
+    }
+
+    @Override
+    public void smell(){
+        System.out.println("Seal sniffs her daughter seal.");
+    }
+
+    @Override
+    public void hunt(){
+        System.out.println("Seal hunts for squid.");
+    }
+
+
+}
+
+
